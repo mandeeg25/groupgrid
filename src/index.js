1023b5a8dec7d8c7888de2f12353d34d3112d96f
Braden Kartchner <braden.kartchner@gmail.com>
2026-07-16 12:00:00 -0600

    install vite and remove react scripts

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GroupGrid from "./GroupGrid";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<GroupGrid />);
